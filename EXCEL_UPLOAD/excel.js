6c7d1d10cffd5580ad95ef67d94022f5c697e456
Atlas <63521376+AtlasServices@users.noreply.github.com>
2022-05-31 17:24:10 +0300

    Update excel.js

--- EXCEL_UPLOAD/excel.js
+++ EXCEL_UPLOAD/excel.js
@@ -343,7 +343,7 @@
 				  var rec_count = 0;
 
 				  var len = 0;
-				  if (lengthfield === 9) {
+				  if (lengthfield != 0) {
 					for (var i = 1; i < result.split("[$@~!~@$]").length; i++) {
 					  if (result.split("[$@~!~@$]")[i].length > 0) {
 
